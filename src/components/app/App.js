bc65f6f7dbb1234176e76ea3a48cc29865789387
Mixail Kirilin <mixkirilin@mail.ru>
2024-04-30 09:50:21 +0300

    Изменение в app файле

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -5,27 +5,53 @@ import { BurgerConstructor } from "../burger-constructor/Burger-constructor";
 import styles from "./styles.module.css";
 
 function App() {
-  const ingredientsURL = 'https://norma.nomoreparties.space/api/ingredients';
-  const [state, setState] = React.useState({
+  const ingredientsURL = "https://norma.nomoreparties.space/api/ingredients";
+  const [ingredientsData, setIngredientsData] = React.useState({
     data: [],
     isLoading: true,
     hasError: false,
   });
 
   React.useEffect(() => {
-    setState({ ...state, hasError: false, isLoading: true });
+    setIngredientsData({
+      ...ingredientsData,
+      hasError: false,
+      isLoading: true,
+    });
     fetch(ingredientsURL)
-      .then((res) => res.json())
-      .then((data) =>
-        setState({ ...state, data, isLoading: false, hasError: false })
-      )
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Ответ сети был не ok.");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        if (data.success) {
+          setIngredientsData({
+            ...ingredientsData,
+            data,
+            isLoading: false,
+            hasError: false,
+          });
+        } else {
+          setIngredientsData({
+            ...ingredientsData,
+            hasError: true,
+            isLoading: false,
+          });
+        }
+      })
       .catch((e) => {
-        setState({ ...state, hasError: true, isLoading: false });
+        setIngredientsData({
+          ...ingredientsData,
+          hasError: true,
+          isLoading: false,
+        });
       });
-      // eslint-disable-next-line
+    // eslint-disable-next-line
   }, []);
 
-  const { data, isLoading, hasError } = state;
+  const { data, isLoading, hasError } = ingredientsData;
   return (
     <>
       <div className={styles.app}>
