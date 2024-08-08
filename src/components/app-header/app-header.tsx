996627a5ad7b715eb4bd8b48d7771ca0f7bdb22d
Mixail Kirilin <mixkirilin@mail.ru>
2024-08-08 07:17:43 +0300

    Логотип ведет на главную

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -6,6 +6,7 @@ import {
 } from "@ya.praktikum/react-developer-burger-ui-components";
 import ButtonHeader from "../button-header/button-header";
 import styles from "./styles.module.css";
+import { Link } from "react-router-dom";
 
 function AppHeader() {
   return (
@@ -18,7 +19,9 @@ function AppHeader() {
       </div>
       <div className={styles.center}>
         {/** @ts-ignore */}
-        <Logo className="logo" />
+        <Link className="logo" to="/">
+          <Logo />
+        </Link>
       </div>
       <div className={styles.end}>
         {/** @ts-ignore */}
