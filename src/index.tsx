2ce1a94d03646896312163229694b244d7950476
Mixail Kirilin <mixkirilin@mail.ru>
2024-09-15 19:43:30 +0300

    Перевод на gh pages

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./components/app/app";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store from "./services/store";
